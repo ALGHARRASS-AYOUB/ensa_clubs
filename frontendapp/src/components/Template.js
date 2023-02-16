69f6938bd41dae2849a2181abb9d3105b2ef2bce
Rezki-Nisrine <nisrine.rezki@usmba.ac.ma>
2023-02-16 22:14:47 +0100

    interface de responsable de club

--- frontendapp/src/components/Template.js
+++ frontendapp/src/components/Template.js
@@ -19,10 +19,10 @@ export default function Template() {
              
               </div>
               <div class="nav-profile-text d-flex flex-column pr-3">
-                <span class="font-weight-medium mb-2">admin </span>
-                <span class="font-weight-normal">$8,753.00</span>
+                <span class="font-weight-medium mb-2"> Responsable</span>
+                <span class="font-weight-normal"> ID_</span>
               </div>
-              <span class="badge badge-danger text-white ml-3 rounded">3</span>
+              <span class="badge badge-danger text-white ml-3 rounded"></span>
             </a>
           </li>
           <li class="nav-item">
@@ -34,47 +34,40 @@ export default function Template() {
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
               <i class="mdi mdi-crosshairs-gps menu-icon"></i>
-              <span class="menu-title">Basic UI Elements</span>
+              <span class="menu-title">Réservations</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="ui-basic">
               <ul class="nav flex-column sub-menu">
                 <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a>
+                  <a class="nav-link" href="pages/ui-features/buttons.html">Réserver une salle </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a>
+                  <a class="nav-link" href="pages/ui-features/dropdowns.html">Salles disponibles</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/typography.html">Typography</a>
-                </li>
+               
               </ul>
             </div>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="pages/icons/mdi.html">
               <i class="mdi mdi-contacts menu-icon"></i>
-              <span class="menu-title">Icons</span>
+              <span class="menu-title">Gestion des Memberes</span>
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="pages/forms/basic_elements.html">
               <i class="mdi mdi-format-list-bulleted menu-icon"></i>
-              <span class="menu-title">Forms</span>
+              <span class="menu-title">Evènements</span>
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="pages/charts/chartjs.html">
               <i class="mdi mdi-chart-bar menu-icon"></i>
-              <span class="menu-title">Charts</span>
+              <span class="menu-title">Suivi d'activité</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="pages/tables/basic-table.html">
-              <i class="mdi mdi-table-large menu-icon"></i>
-              <span class="menu-title">Tables</span>
-            </a>
-          </li>
+         
           <li class="nav-item">
             <span class="nav-link" href="#">
               <span class="menu-title">Docs</span>
@@ -219,7 +212,7 @@ export default function Template() {
               <li class="nav-item nav-profile dropdown border-0">
                 <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-toggle="dropdown">
                   <img class="nav-profile-img mr-2" alt="" src="assets/images/faces/face1.jpg" />
-                  <span class="profile-name">Henry Klein</span>
+                  <span class="profile-name">User</span>
                 </a>
                 <div class="dropdown-menu navbar-dropdown w-100" aria-labelledby="profileDropdown">
                   <a class="dropdown-item" href="#">
